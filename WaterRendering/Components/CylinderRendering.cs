da73be2ea4ac5fa0a1c9f580f1ac190e93bd7ce4
Lukas Messmer <lukas.messmer@ost.ch>
2022-10-19 16:17:24 +0200

    Added example code

---
+++ WaterRendering/Components/CylinderRendering.cs
@@ -0,0 +1,27 @@
+﻿using SharpGfx;
+using SharpGfx.OpenGL.Shading;
+using SharpGfx.Primitives;
+
+namespace S2A2
+{
+    public class CylinderRendering : CameraRendering
+    {
+        public CylinderRendering(Device device, IVector2 size, Camera camera)
+            : base(device, size, device.Color3(0, 0, 0), camera)
+        {
+            const int faceCount = 64;
+            
+            var material = new UniformMaterial(device, device.Color4(1, 0, 0, 1));
+            var vertices = Cylinder.GetVertices(device.Model(), 4, 1, faceCount);
+            var vertexAttribute = new VertexAttribute("positionIn", vertices, 3);
+            var cylinder = device.Object(device.World, "cylinder", material, vertexAttribute);
+            
+            Scene.Add(cylinder);
+
+            //Scene.Add(cylinder.Copy("1")); // TODO
+            //Scene.Add(cylinder.Copy("2")); // TODO
+            //Scene.Add(cylinder.Copy("3")); // TODO
+            //Scene.Add(cylinder.Copy("4")); // TODO
+        }
+    }
+}
--- WaterRendering/Program.cs
+++ WaterRendering/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using S2A2;
+using SharpGfx;
+using SharpGfx.OpenTK;
+
+var device = new OtkDevice();
+var size = device.World.Vector2(1024, 768);
+
+var camera = new OrbitCamera(device.World, device.World.Origin3(), 5);
+var window = new OtkWindow("S2A2", size, camera);
+var rendering = new CylinderRendering(device, size, camera);;
+
+window.Show(rendering);
