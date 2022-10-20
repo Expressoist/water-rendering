f5eae73c706f59667c8f685df71802d4270449d8
Lukas Messmer <lukas.messmer@ost.ch>
2022-10-20 15:30:13 +0200

    Added cosinus surface

---
+++ WaterRendering/Components/RippleRendering.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using SharpGfx;
+using SharpGfx.OpenGL.Shading;
+using SharpGfx.Primitives;
+
+namespace WaterRendering.Components;
+
+public class RippleRendering : CameraRendering
+{
+    
+    public double Time { get; set; }
+
+    private RenderObject _surfaceInstance;
+    
+    private const string SurfaceInstanceName = "";
+    private const int NumberOfSubdivisions = 100;
+    
+    private const float Gravity = 9.8f;
+    private const float Density = 997;
+    private const float SurfaceTension = 73E-3f;
+    private const float Strength = 0.01f;
+
+    private const float DampingCoefficient = 0;
+    
+    public RippleRendering(Device device, IVector2 size, Camera camera)
+        : base(device, size, device.Color3(0, 0, 0), camera)
+    {
+        float[] vertices = GetVerticesOfSurface(NumberOfSubdivisions);
+        
+        _surfaceInstance = device.Object
+        (
+            device.World,
+            SurfaceInstanceName,
+            new UniformMaterial(device, device.Color4(1f, 1f, 0.8f, 1f)),
+            new VertexAttribute("positionIn", vertices, 3)
+            );
+        
+        Scene.Add(_surfaceInstance);
+    }
+
+    private float[] GetVerticesOfSurface(int nOfSubdivisions)
+    {
+        //var vertices = new float[nOfSubdivisions * nOfSubdivisions * 3];
+        var vertices = new List<float>();
+        float offset = nOfSubdivisions / 2;
+
+        for (var x = 0; x < nOfSubdivisions - 1; x++)
+        {
+            for (var y = 0; y < nOfSubdivisions - 1; y++)
+            {
+                AddSquareAtPosition(vertices, x, y, offset);
+            }
+        }
+
+        return vertices.ToArray();
+    }
+
+    private void AddSquareAtPosition(List<float> vertices, int x, int y, float offset)
+    {
+        AddVertexAtPosition(vertices, x, y, offset);
+        AddVertexAtPosition(vertices, x + 1, y, offset);
+        AddVertexAtPosition(vertices, x, y + 1, offset);
+        
+        AddVertexAtPosition(vertices, x + 1, y + 1, offset);
+        AddVertexAtPosition(vertices, x + 1, y, offset);
+        AddVertexAtPosition(vertices, x, y + 1, offset);
+    }
+
+    private void AddVertexAtPosition(List<float> vertices, int x, int y, float offset)
+    {
+        float distanceToOrigin = MathF.Sqrt((x - offset) * (x - offset) + (y - offset) * (y - offset));
+        
+        vertices.Add(x - offset);
+        vertices.Add(y - offset);
+        vertices.Add(0.5f * MathF.Cos(distanceToOrigin));
+        /*vertices[x + NumberOfSubdivisions * y] = x - offset;
+        vertices[x + NumberOfSubdivisions * y + 1] = y - offset;
+        vertices[x + NumberOfSubdivisions * y + 2] = 0;*/
+    }
+
+    public override void OnUpdateFrame()
+    {
+        base.OnUpdateFrame();
+    }
+}
--- WaterRendering/Program.cs
+++ WaterRendering/Program.cs
@@ -1,14 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using S2A2;
+using OpenTK.Graphics.OpenGL;
 using SharpGfx;
+using SharpGfx.OpenGL.Shading;
 using SharpGfx.OpenTK;
+using WaterRendering.Components;
 
 var device = new OtkDevice();
 var size = device.World.Vector2(1024, 768);
 
-var camera = new OrbitCamera(device.World, device.World.Origin3(), 5);
+var camera = new OrbitCamera(device.World, device.World.Origin3(), 30);
 var window = new OtkWindow("S2A2", size, camera);
-var rendering = new CylinderRendering(device, size, camera);;
+var rendering = new RippleRendering(device, size, camera);
 
 window.Show(rendering);
